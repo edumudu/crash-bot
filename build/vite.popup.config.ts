c39701708567d20b4ae7d16fcd31db0d0ac56635
edumudu <eduardomudutiu@gmail.com>
2024-12-14 00:16:04 -0300

    refactor: use vite-tsconfig-paths plugin and configure package as type: module

--- build/vite.popup.config.ts
+++ build/vite.popup.config.ts
@@ -1,11 +1,15 @@
 import { resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({ reactivityTransform: true })],
+  plugins: [
+    tsconfigPaths(),
+    vue({ reactivityTransform: true }),
+  ],
   base: './',
 
   build: {
